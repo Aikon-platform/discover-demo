12dbe160cc4b5f27402bb7506c5cc2558bbd8b39
Robin Champenois <4519091+Evarin@users.noreply.github.com>
2023-11-29 11:28:36 +0100

    prod update

--- front/demowebsite/settings/prod.py
+++ front/demowebsite/settings/prod.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 SECRET_KEY = ENV("SECRET_KEY")
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': "django.db.backends.postgresql_psycopg2",
+        'NAME': ENV("DB_NAME"),
+        'USER': ENV("DB_USER"),
+        'PASSWORD': ENV("DB_PASSWORD"),
+        'HOST': ENV("DB_HOST"),
+        'PORT': ENV("DB_PORT"),
+    }
+}
