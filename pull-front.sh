a4b1a0181a242aaf5a4a7ba55a6651f8ab973e4e
Robin Champenois <4519091+Evarin@users.noreply.github.com>
2023-12-13 15:07:31 +0100

    Better pull script

--- pull-front.sh
+++ pull-front.sh
@@ -1,8 +1,10 @@
-git pull
-cd front/
-source venv/bin/activate
-pip install -r requirements-prod.txt
-python manage.py migrate
-yes | python manage.py collectstatic
-sudo service apache2 restart
-sudo service dramatiq restart
+(
+    git pull &&
+    cd front/ &&
+    source venv/bin/activate &&
+    pip install -r requirements-prod.txt &&
+    python manage.py migrate &&
+    python manage.py collectstatic --noinput &&
+    sudo service apache2 restart &&
+    sudo service dramatiq restart
+)
