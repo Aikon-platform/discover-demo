2baacd766608204460e194a2a289f5041db46e94
Segolene-Albouy <segolene.albouy@gmail.com>
2024-04-18 14:15:07 +0200

    [EXTRACTION] add queue

--- api/app/extraction/const.py
+++ api/app/extraction/const.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 from ..shared.utils.fileutils import create_dirs_if_not, create_file_if_not
-from ..config.base import ENV, BASE_DIR
-
+from ..config.base import ENV, BASE_DIR, XACCEL_PREFIX, API_DATA_FOLDER
 
 DEMO_NAME = "extraction"
 
@@ -9,7 +8,9 @@ DEMO_NAME = "extraction"
 DEMO_DIR = BASE_DIR / "app" / DEMO_NAME
 LIB_PATH = DEMO_DIR / "lib"
 
-EXT_DATA_FOLDER = Path(ENV("API_DATA_FOLDER", default=f"{DEMO_DIR}/data")) / DEMO_NAME
+EXT_QUEUE = "queue3"  # see docker-confs/supervisord.conf
+
+EXT_DATA_FOLDER = API_DATA_FOLDER / DEMO_NAME
 # EXT_XACCEL_PREFIX = Path(ENV("EXT_XACCEL_PREFIX", default="/media/extraction-results"))
 EXT_RESULTS_PATH = EXT_DATA_FOLDER / "results"
 
