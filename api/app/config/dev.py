317d2f5e979da291a980e02a42fa392dd878b335
Segolene-Albouy <segolene.albouy@gmail.com>
2024-09-25 14:35:23 +0200

    [API] make api a submodule

--- api/app/config/dev.py
+++
@@ -1,4 +0,0 @@
-from .base import *
-
-
-BASE_URL = f"http://localhost:{ENV('API_DEV_PORT', default=5000)}"
--- api/app/config/prod.py
+++
@@ -1,5 +0,0 @@
-from .base import *
-
-USE_NGINX_XACCEL = True
-
-BASE_URL = ENV("PROD_URL")
