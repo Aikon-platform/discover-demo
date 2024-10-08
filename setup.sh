961d376fe61e96182e3aee097626d3d699ba02cc
Segolene-Albouy <segolene.albouy@gmail.com>
2024-10-08 10:55:58 +0200

    [SCRIPT] improve setup script

--- setup.sh
+++ setup.sh
@@ -2,14 +2,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-colorEcho() {
-    Color_Off="\033[0m"
-    Red="\033[1;91m"        # Red
-    Green="\033[1;92m"      # Green
-    Yellow="\033[1;93m"     # Yellow
-    Blue="\033[1;94m"       # Blue
-    Purple="\033[1;95m"     # Purple
-    Cyan="\033[1;96m"       # Cyan
+color_echo() {
+    Color_Off="\[\033[0m\]"
+    Red="\[\033[1;91m\]"
+    Green="\[\033[1;92m\]"
+    Yellow="\[\033[1;93m\]"
+    Blue="\[\033[1;94m\]"
+    Purple="\[\033[1;95m\]"
+    Cyan="\[\033[1;96m\]"
 
     case "$1" in
         "green") echo -e "$Green$2$Color_Off";;
@@ -22,7 +22,7 @@ colorEcho() {
     esac
 }
 
-echoTitle(){
+echo_title(){
     sep_line="========================================"
     len_title=${#1}
 
@@ -41,43 +41,23 @@ echoTitle(){
         fi
     fi
 
-    colorEcho purple "\n\n$sep_line\n$title\n$sep_line"
+    color_echo purple "\n\n$sep_line\n$title\n$sep_line"
 }
-
-echoTitle "DOWNLOADING API SUBMODULE"
-git submodule init
-git submodule update
-
-echoTitle "REQUIREMENTS INSTALL"
-
-colorEcho yellow "\nSystem packages ..."
-sudo apt-get install redis-server python3.10 python3.10-venv python3.10-dev curl
-
-colorEcho yellow "\nAPI virtual env ..."
-python3.10 -m venv api/venv
-api/venv/bin/pip install -r api/requirements.txt
-api/venv/bin/pip install python-dotenv
-
-colorEcho yellow "\nFront virtual env ..."
-python3.10 -m venv front/venv
-front/venv/bin/pip install -r front/requirements.txt
-
-echoTitle "SET UP ENVIRONMENT VARIABLES"
 
 generate_random_string() {
     echo "$(openssl rand -base64 32 | tr -d '/\n')"
 }
 
 prompt_user() {
-    env_var=$(colorEcho 'red' "$1")
+    env_var=$(color_echo 'red' "$1")
     default_val="$2"
     current_val="$3"
     desc="$4"
 
     if [ "$2" != "$3" ]; then
-        default="Press enter for $(colorEcho 'cyan' "$default_val")"
+        default="Press enter for $(color_echo 'cyan' "$default_val")"
     elif [ -n "$current_val" ]; then
-        default="Press enter to keep $(colorEcho 'cyan' "$current_val")"
+        default="Press enter to keep $(color_echo 'cyan' "$current_val")"
         default_val=$current_val
     fi
 
@@ -90,19 +70,6 @@ get_env_value() {
     env_file=$2
     value=$(awk -F= -v param="$param" '/^[^#]/ && $1 == param {gsub(/"/, "", $2); print $2}' "$env_file")
     echo "$value"
-}
-
-get_os() {
-    unameOut="$(uname -s)"
-    case "${unameOut}" in
-        Linux*)     os=Linux;;
-        Darwin*)    os=Mac;;
-        CYGWIN*)    os=Cygwin;;
-        MINGW*)     os=MinGw;;
-        MSYS_NT*)   os=Git;;
-        *)          os="UNKNOWN:${unameOut}"
-    esac
-    echo "${os}"
 }
 
 update_env() {
@@ -138,29 +105,10 @@ update_env() {
     done
 }
 
-API_ENV="$SCRIPT_DIR"/api/.env
-FRONT_ENV="$SCRIPT_DIR"/front/.env
-DEV_ENV="$SCRIPT_DIR"/.env.dev
-
-cp "$API_ENV".template "$API_ENV"
-cp "$FRONT_ENV".template "$FRONT_ENV"
-cp "$DEV_ENV".template "$DEV_ENV"
-
-colorEcho yellow "\nSetting $API_ENV ..."
-update_env "$API_ENV"
-
-. "$API_ENV"
-
-if [ "$TARGET" == "dev" ]; then
-    echoTitle "PRE-COMMIT INSTALL"
-    api/venv/bin/pip install pre-commit
-    pre-commit install
-fi
-
 set_redis() {
     redis_psw="$1"
     REDIS_CONF=$(redis-cli INFO | grep config_file | awk -F: '{print $2}' | tr -d '[:space:]')
-    colorEcho yellow "\n\nModifying Redis configuration file $REDIS_CONF ..."
+    color_echo yellow "\n\nModifying Redis configuration file $REDIS_CONF..."
 
     # use the same redis password for api and front
     sed -i '' -e "s~^REDIS_PASSWORD=.*~REDIS_PASSWORD=\"$redis_psw\"~" "$FRONT_ENV"
@@ -171,47 +119,79 @@ set_redis() {
     sudo systemctl restart redis
     # brew services restart redis # MacOs
 }
-# NOTE uncomment to use Redis password
-# set_redis $REDIS_PASSWORD
 
-colorEcho yellow "\nSetting $FRONT_ENV file ..."
-update_env "$FRONT_ENV"
-
-echoTitle "DOWNLOADING API SUBMODULES (dticlustering, etc.)"
+echo_title "DOWNLOADING API SUBMODULE"
 git submodule init
 git submodule update
+cd api && git submodule init && git submodule update && cd ..
 
-echoTitle "INITIALIZE DJANGO"
 
+echo_title "REQUIREMENTS INSTALL"
+color_echo yellow "\nSystem packages..."
+sudo apt-get install redis-server python3.10 python3.10-venv python3.10-dev curl
+
+color_echo yellow "\nAPI virtual env..."
+python3.10 -m venv api/venv
+api/venv/bin/pip install --upgrade pip
+api/venv/bin/pip install -r api/requirements.txt
+api/venv/bin/pip install python-dotenv
+
+color_echo yellow "\nFront virtual env..."
+python3.10 -m venv front/venv
+front/venv/bin/pip install --upgrade pip
+front/venv/bin/pip install -r front/requirements.txt
+
+
+echo_title "SET UP ENVIRONMENT VARIABLES"
+API_ENV="$SCRIPT_DIR"/api/.env
+FRONT_ENV="$SCRIPT_DIR"/front/.env
+DEV_ENV="$SCRIPT_DIR"/.env.dev
+cp "$API_ENV".template "$API_ENV"
+cp "$FRONT_ENV".template "$FRONT_ENV"
+cp "$DEV_ENV".template "$DEV_ENV"
+
+color_echo yellow "\nSetting $API_ENV..."
+update_env "$API_ENV"
+. "$API_ENV"
+if [ "$TARGET" == "dev" ]; then
+    echo_title "PRE-COMMIT INSTALL"
+    api/venv/bin/pip install pre-commit
+    pre-commit install
+fi
+
+# NOTE uncomment to use Redis password
+# color_echo yellow "\nSetting Redis password..."
+# set_redis $REDIS_PASSWORD
+
+color_echo yellow "\nSetting $FRONT_ENV file..."
+update_env "$FRONT_ENV"
+
+
+echo_title "INITIALIZE DJANGO"
 . "$FRONT_ENV"
-
 cd front
 ./venv/bin/python manage.py migrate
 
-colorEcho yellow "\nCreating superuser\nusername: $ADMIN_NAME\nemail: $ADMIN_EMAIL\npassword: <same-as-db-password> ..."
+color_echo yellow "\nCreating superuser\nusername: $ADMIN_NAME\nemail: $ADMIN_EMAIL\npassword: <same-as-db-password>..."
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('$ADMIN_NAME', '$ADMIN_EMAIL', '$DB_PASSWORD')" | ./venv/bin/python manage.py shell
 
-echoTitle "SETUP WEBPACK"
 
+echo_title "SETUP WEBPACK"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # BUG not working from there
-
 nvm install node
 sudo apt install npm
-
 npm install -g webpack
 npm install -g sass
 npm install -D webpack-cli
-
 cd webpack
-colorEcho yellow "\nInit npm project inside $(pwd) ..."
+
+color_echo yellow "\nInit npm project inside $(pwd)..."
 npm init
-
 npm install
-
 npm run start
 npm run production
