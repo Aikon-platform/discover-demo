485355d147ceb3d1d66fca042e7f5d41083b1cfa
Segolene-Albouy <segolene.albouy@gmail.com>
2025-01-07 11:33:32 +0100

    [DATA] remove directory creation

--- front/datasets/apps.py
+++ front/datasets/apps.py
@@ -1,8 +1,5 @@
-import os
-
 from django.apps import AppConfig
 
-from demowebsite import settings
 
 """
 This app contains the models and views related to the datasets,
@@ -11,11 +8,5 @@ that may be shared in future applications independent from the DTI clustering
 
 
 class DatasetsConfig(AppConfig):
-    def ready(self):
-        datasets_dir = os.path.join(settings.MEDIA_ROOT, "datasets")
-        documents_dir = os.path.join(settings.MEDIA_ROOT, "documents")
-        os.makedirs(datasets_dir, exist_ok=True)
-        os.makedirs(documents_dir, exist_ok=True)
-
     default_auto_field = "django.db.models.BigAutoField"
     name = "datasets"
