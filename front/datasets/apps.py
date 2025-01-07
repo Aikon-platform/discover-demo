b40396acdf1313981ee5e5debc1f5ae2216c1e45
Segolene-Albouy <segolene.albouy@gmail.com>
2025-01-07 10:29:44 +0100

    [DATA] ensure data dir exists when the app is launched

--- front/datasets/apps.py
+++ front/datasets/apps.py
@@ -1,10 +1,21 @@
+import os
+
 from django.apps import AppConfig
+
+from demowebsite import settings
 
 """
 This app contains the models and views related to the datasets,
 that may be shared in future applications independent from the DTI clustering
 """
 
+
 class DatasetsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'datasets'
+    def ready(self):
+        datasets_dir = os.path.join(settings.MEDIA_ROOT, "datasets")
+        documents_dir = os.path.join(settings.MEDIA_ROOT, "documents")
+        os.makedirs(datasets_dir, exist_ok=True)
+        os.makedirs(documents_dir, exist_ok=True)
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "datasets"
--- pull-front.sh
+++ pull-front.sh
@@ -4,11 +4,15 @@ source "$ROOT_DIR/scripts/utils.sh"
 (
     git pull &&
     cd front/ &&
+    color_echo "yellow" "\nPython dependencies update" &&
     source venv/bin/activate &&
+    pip install --upgrade pip &&
     pip install -r requirements-prod.txt &&
+    color_echo "yellow" "\nDjango database migration & static collection" &&
     python manage.py migrate &&
     python manage.py collectstatic --noinput &&
+    color_echo "yellow" "\nWeb server restart" &&
     sudo service apache2 restart &&
     sudo service dramatiq restart &&
-    color_echo "green" "Update successful!"
+    color_echo "green" "\n\nUpdate successful!"
 )
