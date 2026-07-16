a3a1081e4cf2f2f07e1a4dd32e34a19bde554fb9
Nassima OULD OUALI <nassima.ould-ouali@ip-paris.fr>
2026-07-16 11:59:35 +0200

    paleography: add views via task_view_set

---
+++ front/paleography/views.py
@@ -0,0 +1,21 @@
+from .forms import PaleographyForm
+from .models import Paleography
+from tasking.views import task_view_set
+
+
+# Génère les vues Start / Status / Progress / Cancel / Delete / List ... (cf. regions)
+@task_view_set
+class PaleographyMixin:
+    """
+    Mixin pour les vues Paléographie.
+    """
+
+    model = Paleography
+    form_class = PaleographyForm
+    task_name = "Paléographie"
+    app_name = "paleography"
+    # NOTE: task_data="dataset" pour utiliser le template de formulaire dataset
+    task_data = "dataset"
+
+# [ÉTAPE SUIVANTE] La page de visu (images à gauche / transcriptions à droite)
+# se fera en surchargeant Status, sur le modèle de RegionsStatus.
