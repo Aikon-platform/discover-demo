9f4568f4852e3b30c9f2a0a5d8f8ebf3f6aaf5ba
Nassima OULD OUALI <nassima.ould-ouali@ip-paris.fr>
2026-07-20 17:50:22 +0200

    paleography: check 'has transcriptions' by default

--- front/paleography/forms.py
+++ front/paleography/forms.py
@@ -7,7 +7,8 @@ from tasking.forms import AbstractTaskOnDatasetForm
 class PaleographyForm(AbstractTaskOnDatasetForm):
     """
     Formulaire réduit à la SEULE partie dataset-upload (décision réunion #9),
-    restreint au format Zip (décision #4).
+    restreint au format Zip (décision #4), avec les transcriptions attendues
+    par défaut (le module paléographie travaille sur des datasets transcrits).
     """
 
     class Meta(AbstractTaskOnDatasetForm.Meta):
@@ -16,11 +17,16 @@ class PaleographyForm(AbstractTaskOnDatasetForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         # Décision #4 : paléographie n'accepte que le Zip.
         # `format` est un ChoiceField dont les choix sont posés par
         # AbstractDatasetForm.__init__ ; on les restreint à la seule entrée "zip".
         self.fields["format"].choices = [
             choice for choice in self.fields["format"].choices if choice[0] == "zip"
         ]
-        # valeur par défaut = zip (un seul choix, autant le pré-sélectionner)
         self.fields["format"].initial = "zip"
+
+        # Checkbox "has transcriptions" cochée par défaut dans ce module
+        # (un dataset paléographie est censé porter ses transcriptions).
+        if "has_transcriptions" in self.fields:
+            self.fields["has_transcriptions"].initial = True
